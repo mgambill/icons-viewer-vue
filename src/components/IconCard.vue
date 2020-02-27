81de1e38d7fdb440ff2a94fb7a65923ff62db764
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-02-27 00:52:18 -0500

    use proper semantic

--- src/components/IconCard.vue
+++ src/components/IconCard.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div(class="w-full sm:w-1/4 md:w-1/5 xl:w-1/6")
   div(class="m-2")
-    div(class="relative flex-shrink-0 flex flex-col justify-center items-center w-full h-40 text-gray-800 hover:text-white bg-white hover:bg-indigo-500 rounded shadow hover:shadow-lg hover:z-30 transition duration-150 ease-in-out"  @click="doCopy")
+    a(href="" class="relative flex-shrink-0 flex flex-col justify-center items-center w-full h-40 text-gray-800 hover:text-white bg-white hover:bg-indigo-500 rounded shadow hover:shadow-lg hover:z-30 transition duration-150 ease-in-out"  @click.prevent="doCopy")
       .w-5.h-5
         component(:is="`${type}${icon.icon}`" ref="s")
         textarea(v-show="false") {{ markup }}
