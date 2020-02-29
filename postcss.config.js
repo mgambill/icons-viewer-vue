3b156f01d4a415db01faf1c79a0c12953dd5f4a4
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-02-29 10:45:32 -0500

    audit tweaks and added purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")]
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: [`./public/**/*.html`, `./src/**/*.vue`],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          ""
+        );
+        return (
+          contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) ||
+          []
+        );
+      },
+      whitelist: [],
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/
+      ]
+    })
+  ]
 };
