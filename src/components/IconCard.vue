b577fe45d13bdc8252c1a63f5d75c0cf22623f17
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-03-04 00:45:31 -0500

    icon card tweaks

--- src/components/IconCard.vue
+++ src/components/IconCard.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 div(:class="[iconSize < 24 ? 'w-full sm:w-1/4 md:w-1/5 xl:w-1/6' : 'w-1/4' ]")
   div(class="m-2")
-    a(href="" :class="[isDark? 'bg-cool-gray-700 text-cool-gray-100' : 'bg-white' , iconSize < 32 ? 'h-40' : iconSize < 64 ?' h-64' : 'h-72' ]" class="relative flex-shrink-0 flex flex-col justify-center items-center w-full text-gray-800 hover:text-white  hover:bg-indigo-500 rounded shadow hover:shadow-lg hover:z-30 transition duration-150 ease-in-out"  @click.prevent="doCopy")
+    a(href="" :class="[isDark? 'bg-cool-gray-700 text-cool-gray-100' : 'bg-white' , iconSize < 32 ? 'h-40' : iconSize < 56 ?' h-64' : 'h-72' ]" class="relative flex-shrink-0 flex flex-col justify-center items-center w-full text-gray-800 hover:text-white  hover:bg-indigo-500 rounded shadow hover:shadow-lg hover:z-30 transition duration-150 ease-in-out"  @click.prevent="doCopy")
       div()
         component(:is="`${type}${icon.icon}`" ref="s" :strokeWidth="iconStrokeWidth" :class="`w-${iconSize} h-${iconSize}`")
         textarea(v-show="false") {{ markup }}
-      div.p-3.absolute.x-inset-0.bottom-0
+      div.p-5.absolute.x-inset-0.bottom-0
         p.font-semibold.text-center.text-xs.leading-tight {{ title }}
 </template>
 
