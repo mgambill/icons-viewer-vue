798656e105e934e046719d2c8bd8391e6e970630
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-03-04 21:50:03 -0500

    revert purge regex

--- postcss.config.js
+++ postcss.config.js
@@ -2,20 +2,20 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
-    process.env.NODE_ENV === "production" &&
-      require("@fullhuman/postcss-purgecss")({
-        content: [`./public/**/*.html`, `./src/**/*.vue`],
-        defaultExtractor(content) {
-          const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
-          return contentWithoutStyleBlocks.match(/[\w-/.:]+(?<!:)]/g) || [];
-        },
-        whitelist: [],
-        whitelistPatterns: [
-          /-(leave|enter|appear)(|-(to|from|active))$/,
-          /^(?!(|.*?:)cursor-move).+-move$/,
-          /^router-link(|-exact)-active$/,
-          /^w-|^h-/
-        ]
-      })
+
+    require("@fullhuman/postcss-purgecss")({
+      content: [`./public/**/*.html`, `./src/**/*.vue`],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, "");
+        return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || [];
+      },
+      whitelist: [],
+      whitelistPatterns: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /^w-|^h-/
+      ]
+    })
   ]
 };
