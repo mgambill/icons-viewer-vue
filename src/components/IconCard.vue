69bfafea321e3dda6d5092875af53cb6f9ac445a
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-03-04 22:10:19 -0500

    increase the height on h-64

--- src/components/IconCard.vue
+++ src/components/IconCard.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div(:class="[iconSize < 24 ? 'w-full sm:w-1/4 md:w-1/5 xl:w-1/6' : 'w-1/4' ]")
   div(class="m-2")
-    a(href="" :class="[isDark? 'bg-cool-gray-700 text-cool-gray-100' : 'bg-white' , iconSize < 32 ? 'h-40' : iconSize < 56 ?' h-64' : 'h-72' ]" class="relative flex-shrink-0 flex flex-col justify-center items-center w-full text-gray-800 hover:text-white  hover:bg-indigo-500 rounded shadow hover:shadow-lg hover:z-30 transition duration-150 ease-in-out"  @click.prevent="doCopy")
+    a(href="" :class="[isDark? 'bg-cool-gray-700 text-cool-gray-100' : 'bg-white' , iconSize < 32 ? 'h-40' : iconSize < 56 ?' h-64' : 'h-80' ]" class="relative flex-shrink-0 flex flex-col justify-center items-center w-full text-gray-800 hover:text-white  hover:bg-indigo-500 rounded shadow hover:shadow-lg hover:z-30 transition duration-150 ease-in-out"  @click.prevent="doCopy")
       div()
         component(:is="`${type}${icon.icon}`" ref="s" :strokeWidth="iconStrokeWidth" :class="`w-${iconSize} h-${iconSize}`")
         textarea(v-show="false") {{ markup }}
